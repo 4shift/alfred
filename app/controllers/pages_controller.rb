ee6bdacde308c7d5638b99dabb03873165b71de6
wecanooo <wecanooo@4shift.com>
2015-12-02 15:48:31 +0900

    landing page 작업완료

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,5 +3,13 @@ class PagesController < ApplicationController
   skip_authorization_check
   include HighVoltage::StaticPage
 
+  before_action :prepare_signup
+
   layout "landing"
+
+  private
+
+  def prepare_signup
+    @signup = Signup.new
+  end
 end
