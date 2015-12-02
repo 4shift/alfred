f8dc872a99ab42a78c93507604df66e5dfcfb294
wecanooo <wecanooo@4shift.com>
2015-12-03 08:59:45 +0900

    change assets precompile config

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -8,12 +8,12 @@ Rails.application.config.assets.version = '1.0'
 Rails.application.config.assets.paths << "#{Rails.root}/vendor/assets/*"
 Rails.application.config.assets.paths << "#{Rails.root}/vendor/assets/fonts"
 Rails.application.config.assets.paths << "#{Rails.root}/vendor/assets/stylesheets"
+
+# Precompile additional assets.
+# application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
+Rails.application.config.assets.precompile += %w( application-print.css chat.js users.js alfred-plugin.js brimir-plugin.css landing.css )
 Rails.application.config.assets.precompile << Proc.new { |path|
   if path =~ /\.(eot|svg|ttf|woff|otf)\z/
     true
   end
 }
-
-# Precompile additional assets.
-# application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-Rails.application.config.assets.precompile += %w( application-print.css chat.js users.js alfred-plugin.js brimir-plugin.css landing.css )
