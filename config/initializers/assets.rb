a1b2a229392d6e7c6d7d2e8b9350066605f47f4e
wecanooo <wecanooo@4shift.com>
2015-12-03 08:52:29 +0900

    add assets pipeline configuration

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -5,7 +5,13 @@ Rails.application.config.assets.version = '1.0'
 
 # Add additional assets to the asset load path
 # Rails.application.config.assets.paths << Emoji.images_path
+Rails.application.config.assets.paths << "#{Rails.root}/vendor/assets/*"
 Rails.application.config.assets.paths << "#{Rails.root}/vendor/assets/fonts"
+Rails.application.config.assets.paths << "#{Rails.root}/vendor/assets/stylesheets"
+Rails.application.config.assets.precompile << Proc.new { |path|
+  if path =~ /\.(eot|svg|ttf|woff|otf)\z/
+    true
+  end
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
